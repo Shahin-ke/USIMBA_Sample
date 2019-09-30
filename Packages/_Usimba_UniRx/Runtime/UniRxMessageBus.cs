b9f3f146fd649ea93c4bc84e33a31d30c64a4ddd
Shahin <Shahin_ke@hotmail.com>
2019-09-30 10:13:08 +0330

    Move from other projects, convert to new style package and minor cleanup

---
+++ Packages/_Usimba_UniRx/Runtime/UniRxMessageBus.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SH_SWAT.Usimba.EventOrientedModule.CQRS;
+using SH_SWAT.Usimba.Validation;
+using UniRx;
+using UnityEngine;
+
+namespace SH_SWAT.Usimba.MessageBus.UniRxExtension
+{
+    public class UniRxMessageBus : IMessageBus
+    {
+        private readonly bool _assertWarnings;
+
+        // ReSharper disable once InconsistentNaming
+        public readonly Subject<IMessage> _messages = new Subject<IMessage>();
+        public IObservable<IMessage> Messages => _messages;
+
+        public List<MessageRouteRule> Rules = new List<MessageRouteRule>();
+        public SubscriptionRegistery RuleSubscriptions = new SubscriptionRegistery();
+
+        private readonly List<RegisteryItem> _subscribedHandlerWithoutRule = new List<RegisteryItem>();
+
+        public UniRxMessageBus(bool assertWarnings)
+        {
+            _assertWarnings = assertWarnings;
+        }
+
+        public void RaiseEvent(IEvent evt)
+        {
+            if (null == evt)
+            {
+                return;
+            }
+
+            _messages.OnNext(evt);
+        }
+
+        public void SendCommand(ICommand cmd)
+        {
+            if (null == cmd)
+            {
+                return;
+            }
+
+            var validators = GetValidators(cmd);
+            var validationContext = new ValidationContext();
+
+            var lastValidatorResult = Usimba.Validation.ValidationResult.Accepted;
+            for(var i = 0 ; i < validators.Count && lastValidatorResult == ValidationResult.Accepted; ++i)
+            {
+                var validator = validators[i];
+                lastValidatorResult = validator.IsValid(cmd, validationContext);
+            }
+
+            if (ValidationResult.Accepted == lastValidatorResult)
+            {
+                _messages.OnNext(cmd);
+            }
+        }
+
+        private List<IValidator> GetValidators(ICommand cmd)
+        {
+            //var cmdType = cmd.GetType();
+
+            // TODO: Should find validators by validation or settings
+
+            return new List<IValidator>();
+        }
+
+        public void Subscribe<TMessageHandler, TMessage>(TMessageHandler handler, IMessageHandlerActionExecutor methodSelector)
+            where TMessage : class, IMessage
+            where TMessageHandler : class, IMessageHandler<TMessage>
+        {
+
+            Subscribe(handler, typeof(TMessage), methodSelector);
+        }
+
+        private void Subscribe(IMessageHandler handler, Type messageType, IMessageHandlerActionExecutor methodSelector)
+        {
+            var messageHandlerType = handler.GetType();
+            var rules = GetRule(messageHandlerType, messageType);
+
+            var conditionResult = null != rules && rules.Count > 0;
+            if (_assertWarnings)
+            {
+                UnityEngine.Assertions.Assert.IsTrue(conditionResult,
+                    $"Message Rule for {messageHandlerType.Name}<{messageType}> Not Found.");
+            }
+
+            if (conditionResult)
+            {
+                foreach (var rule in rules)
+                {
+                    var items = RuleSubscriptions.ItemsByRule(rule);
+                    var item = items.FirstOrDefault(it => it.Handler == handler);
+                    if (null == item)
+                    {
+                        IObservable<IMessage> observable;
+
+                        if (null != rule.Transformer)
+                        {
+                            observable =
+                                Messages.Where(
+                                    message =>
+                                        rule.IncludeDerivedMessageTypes
+                                            ? rule.Transformer.InputType.IsInstanceOfType(message)
+                                            : message.GetType() == rule.Transformer.InputType);
+
+                            if (null != rule.PreCondition)
+                            {
+                                observable = observable.Where(message => EventOrientedModule.CQRS.Validators.ValidationResult.Accepted == rule.PreCondition.Validate(handler, message));
+                            }
+                            observable = observable.Select(message => Convert.ChangeType(rule.Transformer.Transform(message), rule.Transformer.OutputType) as IMessage);
+                        }
+                        else
+                        {
+                            observable = Messages.Where(message => rule.IncludeDerivedMessageTypes ? messageType.IsInstanceOfType(message) : messageType == message.GetType());
+                        }
+
+                        if (null != rule.PostCondition)
+                        {
+                            observable = observable.Where(message => EventOrientedModule.CQRS.Validators.ValidationResult.Accepted == rule.PostCondition.Validate(handler, message));
+                        }
+
+                        var subscription = observable.Subscribe(message => methodSelector.Execute(message));
+
+                        RuleSubscriptions.Add(new SubscriptionRegisteryItem()
+                        {
+                            Route = rule,
+                            Handler = handler,
+                            MethodInfo = methodSelector,
+                            MessagehandlerType = messageHandlerType,
+                            MessageType = messageType,
+                            Subscription = subscription
+                        });
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Duplicate Subscription.");
+                    }
+                }
+            }
+            else
+            {
+                var registeryItem = new RegisteryItem(messageType, handler, methodSelector);
+                _subscribedHandlerWithoutRule.Add(registeryItem);
+            }
+        }
+
+        public void Unsubscribe<TMessageHandler, TMessage>(TMessageHandler handler)
+            where TMessage : class, IMessage
+            where TMessageHandler : class, IMessageHandler<TMessage>
+        {
+            var rule = GetRule<TMessageHandler, TMessage>();
+
+            UnityEngine.Assertions.Assert.IsNotNull(rule, $"Message Rule for {typeof(TMessageHandler)}<{typeof(TMessage)}> Not Found.");
+            if (null == rule) return;
+
+            var subscriptions = RuleSubscriptions.GetRuleSubscriptions(rule)?.ToList();
+            if (null == subscriptions || subscriptions.Count <= 0) return;
+
+            var handlerSubscriptions = subscriptions.Where(item => item.Handler == handler).ToList();
+            var handlerSubscriptionsCount = handlerSubscriptions.Count;
+            if (handlerSubscriptionsCount <= 0) return;
+
+            for (var i = handlerSubscriptionsCount - 1; i >= 0; --i)
+            {
+                var subscription = subscriptions[i];
+                subscription.Subscription.Dispose();
+                subscriptions.Remove(subscription);
+            }
+        }
+
+        public void AddRule(MessageRouteRule rule)
+        {
+            Rules.Add(rule);
+
+            //var items =
+            //    _subscribedHandlerWithoutRule.Where(item => item.HandlerType == rule.HandlerType && item.MessageType == rule.MessageType)
+            //        .ToList();
+
+            var items =
+                _subscribedHandlerWithoutRule.Where(
+                        item => IsTypeAcceptedByRule(rule, item.HandlerType, item.MessageType))
+                    .ToList();
+
+            if (items.Count <= 0)
+            {
+                return;
+            }
+
+            for (var i = items.Count - 1; i >= 0; --i)
+            {
+                var item = items[i];
+
+                dynamic objHandler;
+                var targetAvaliable = item.Handler.TryGetTarget(out objHandler);
+                if (!targetAvaliable) continue;
+
+                Subscribe(objHandler, item.MessageType, item.HandlerMethodSelector);
+
+                items.Remove(item);
+            }
+        }
+
+        private MessageRouteRule GetRule<TMessageHandler, TMessage>() where TMessage : IMessage where TMessageHandler : IMessageHandler<TMessage>
+        {
+            return Rules.FirstOrDefault(
+                roteRule => typeof(TMessageHandler) == roteRule.HandlerType && typeof(TMessage) == roteRule.MessageType);
+        }
+
+        private List<MessageRouteRule> GetRule(Type messageHandlerType, Type messageType)
+        {
+            return Rules.Where(
+                roteRule => IsTypeAcceptedByRule(roteRule, messageHandlerType, messageType))
+                .ToList();
+        }
+
+        private bool IsTypeAcceptedByRule(MessageRouteRule rule, Type messageHandlerType, Type messageType)
+        {
+            return (rule.IncludeDerivedHandlerTypes
+                       ? rule.HandlerType.IsAssignableFrom(messageHandlerType)
+                       : messageHandlerType == rule.HandlerType)
+                   &&
+                   (rule.IncludeDerivedMessageTypes
+                       ? rule.MessageType.IsAssignableFrom(messageType)
+                       : messageType == rule.MessageType);
+        }
+    }
+}
+
---
+++ Packages/_Usimba_UniRx/Tests/Runtime/UniRxMessageBusTests.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Text;
+using SH_SWAT.Usimba.EventOrientedModule.CQRS;
+using SH_SWAT.Usimba.EventOrientedModule.CQRS.Validators;
+using SH_SWAT.Usimba.MessageBus.UniRxExtension;
+using SH_SWAT.Usimba.UnitTests.Editor.MessageBus.UniRxExtension.Handlers;
+using SH_SWAT.Usimba.UnitTests.Editor.MessageBus.UniRxExtension.Messages;
+using SH_SWAT.Usimba.UnitTests.Editor.MessageBus.UniRxExtension.Transformers;
+using SH_SWAT.Usimba.UnitTests.Editor.MessageBus.UniRxExtension.Validators;
+using NUnit.Framework;
+
+namespace SH_SWAT.Usimba.UnitTests.Editor.MessageBus.UniRxExtension
+{
+    [TestFixture]
+    public class UniRxMessageBusTests
+    {
+        [SetUp]
+        public void BindInterfaces()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void When_Subscribe_After_Add_Rule_Should_Handler_Execute()
+        {
+            var messagebus = new UniRxMessageBus(true);
+
+            var handler = new SomeMessageHandler(string.Empty);
+
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(string.Empty, false, false));
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(handler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => handler.Handle(msg)));
+
+            var message = new SomeMessage("First message", null, DateTime.UtcNow);
+
+            messagebus.RaiseEvent(message);
+
+            Assert.AreEqual(1, handler.HandleCallCount);
+        }
+
+        [Test]
+        public void When_Subscribe_Before_Add_Rule_Should_Handler_Execute()
+        {
+            var messagebus = new UniRxMessageBus(false);
+
+            var handler = new SomeMessageHandler(string.Empty);
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(handler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => handler.Handle(msg)));
+
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(string.Empty, false, false));
+
+            var message = new SomeMessage("First message", null, DateTime.UtcNow);
+
+            messagebus.RaiseEvent(message);
+
+            Assert.AreEqual(1, handler.HandleCallCount);
+        }
+
+        [Test]
+        public void When_Subscribe_Two_Handler_After_Add_A_Null_Condition_Rule_Should_Both_Handler_Execute()
+        {
+            var messagebus = new UniRxMessageBus(true);
+
+            // Define some const
+            const string firstHandlerName = "handler First";
+            const string secondHandlerName = "handler Second";
+            const string firstMessageString = firstHandlerName + "_Message";
+            const string secondMessageString = secondHandlerName + "_Message";
+
+            // Create Messages
+            var firstMessage = new SomeMessage(firstMessageString, null, DateTime.UtcNow);
+            var secondMessage = new SomeMessage(secondMessageString, null, DateTime.UtcNow);
+
+            // Create Handlers
+            var firstHandler = new SomeMessageHandler(firstHandlerName);
+            var secondHandler = new SomeMessageHandler(secondHandlerName);
+
+            // Add Rules
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(firstHandlerName + "_Rule", false, false));
+
+            // Subscribe handlers
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(firstHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => firstHandler.Handle(msg)));
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(secondHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => secondHandler.Handle(msg)));
+
+            // Raise events
+            messagebus.RaiseEvent(firstMessage);
+            messagebus.RaiseEvent(secondMessage);
+
+            // Assertions
+            Assert.AreEqual(2, firstHandler.HandleCallCount, CreateAssetMessage(firstHandler, 0));
+            Assert.AreEqual(2, secondHandler.HandleCallCount, CreateAssetMessage(secondHandler, 1));
+        }
+
+        [Test]
+        public void When_Subscribe_Two_Handler_After_Add_A_True_Condition_Rule_Should_Both_Handler_Execute()
+        {
+            var messagebus = new UniRxMessageBus(true);
+
+            // Define some const
+            const string firstHandlerName = "handler First";
+            const string secondHandlerName = "handler Second";
+            const string firstMessageString = firstHandlerName + "_Message";
+            const string secondMessageString = secondHandlerName + "_Message";
+
+            // Create Messages
+            var firstMessage = new SomeMessage(firstMessageString, null, DateTime.UtcNow);
+            var secondMessage = new SomeMessage(secondMessageString, null, DateTime.UtcNow);
+
+            // Create Handlers
+            var firstHandler = new SomeMessageHandler(firstHandlerName);
+            var secondHandler = new SomeMessageHandler(secondHandlerName);
+
+            // Add Rules
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(firstHandlerName + "_Rule", false, false, new AlwaysConstResultConditionValidator(ValidationResult.Accepted)));
+
+            // Subscribe handlers
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(firstHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => firstHandler.Handle(msg)));
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(secondHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => secondHandler.Handle(msg)));
+
+            // Raise events
+            messagebus.RaiseEvent(firstMessage);
+            messagebus.RaiseEvent(secondMessage);
+
+            // Assertions
+            Assert.AreEqual(2, firstHandler.HandleCallCount, CreateAssetMessage(firstHandler, 0));
+            Assert.AreEqual(2, secondHandler.HandleCallCount, CreateAssetMessage(secondHandler, 1));
+        }
+
+        [Test]
+        public void When_Subscribe_Two_Handler_After_Add_A_False_Condition_Rule_Should_Both_Handler_Execute()
+        {
+            var messagebus = new UniRxMessageBus(true);
+
+            // Define some const
+            const string firstHandlerName = "handler First";
+            const string secondHandlerName = "handler Second";
+            const string firstMessageString = firstHandlerName + "_Message";
+            const string secondMessageString = secondHandlerName + "_Message";
+
+            // Create Messages
+            var firstMessage = new SomeMessage(firstMessageString, null, DateTime.UtcNow);
+            var secondMessage = new SomeMessage(secondMessageString, null, DateTime.UtcNow);
+
+            // Create Handlers
+            var firstHandler = new SomeMessageHandler(firstHandlerName);
+            var secondHandler = new SomeMessageHandler(secondHandlerName);
+
+            // Add Rules
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(firstHandlerName + "_Rule", false, false, new AlwaysConstResultConditionValidator(ValidationResult.Rejected)));
+
+            // Subscribe handlers
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(firstHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => firstHandler.Handle(msg)));
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(secondHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => secondHandler.Handle(msg)));
+
+            // Raise events
+            messagebus.RaiseEvent(firstMessage);
+            messagebus.RaiseEvent(secondMessage);
+
+            // Assertions
+            Assert.AreEqual(0, firstHandler.HandleCallCount, CreateAssetMessage(firstHandler, 0));
+            Assert.AreEqual(0, secondHandler.HandleCallCount, CreateAssetMessage(secondHandler, 1));
+        }
+
+        [Test]
+        public void When_Subscribe_Two_Handler_After_Add_Two_Different_Rule_With_Different_Condition_Should_Handler_Execute()
+        {
+            var messagebus = new UniRxMessageBus(true);
+
+            // Define some const
+            const string firstHandlerName = "handler First";
+            const string secondHandlerName = "handler Second";
+            const string firstMessageString = firstHandlerName + "_Message";
+            const string secondMessageString = secondHandlerName + "_Message";
+
+            // Create Messages
+            var firstMessage = new SomeMessage(firstMessageString, null, DateTime.UtcNow);
+            var secondMessage = new SomeMessage(secondMessageString, null, DateTime.UtcNow);
+
+            // Create Handlers
+            var firstHandler = new SomeMessageHandler(firstHandlerName);
+            var secondHandler = new SomeMessageHandler(secondHandlerName);
+
+            // Add Rules
+            var firstCondition = new SampleConditionValidator(firstHandlerName, firstMessageString);
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(firstHandlerName + "_Rule", false, false, firstCondition));
+
+            var secondCondition = new SampleConditionValidator(secondHandlerName, secondMessageString);
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(secondHandlerName + "_Rule", false, false, secondCondition));
+
+
+            // Subscribe handlers
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(firstHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => firstHandler.Handle(msg)));
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(secondHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => secondHandler.Handle(msg)));
+
+            // Raise events
+            messagebus.RaiseEvent(firstMessage);
+            messagebus.RaiseEvent(secondMessage);
+
+            // Assertions
+            Assert.AreEqual(1, firstHandler.HandleCallCount, CreateAssetMessage(firstHandler, 0));
+            Assert.AreEqual(1, secondHandler.HandleCallCount, CreateAssetMessage(secondHandler, 1));
+        }
+
+        [Test]
+        public void When_Subscribe_Two_Handler_After_Add_A_Rule_Should_Handlers_Execute()
+        {
+            var messagebus = new UniRxMessageBus(true);
+
+            // Define some const
+            const string firstHandlerName = "handler First";
+            const string secondHandlerName = "handler Second";
+            const string firstMessageString = firstHandlerName + "_Message";
+
+            // Create Messages
+            var firstMessage = new SomeMessage(firstMessageString, null, DateTime.UtcNow);
+
+            // Create Handlers
+            var firstHandler = new SomeMessageHandler(firstHandlerName);
+            var secondHandler = new SomeMessageHandler(secondHandlerName);
+
+            // Add Rules
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(firstHandlerName + "_Rule", false, false, null, null, null));
+
+            // Subscribe handlers
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(firstHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => firstHandler.Handle(msg)));
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(secondHandler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => secondHandler.Handle(msg)));
+
+            // Raise events
+            messagebus.RaiseEvent(firstMessage);
+
+            // Assertions
+            Assert.AreEqual(1, firstHandler.HandleCallCount, CreateAssetMessage(firstHandler, 0));
+            Assert.AreEqual(1, secondHandler.HandleCallCount, CreateAssetMessage(secondHandler, 1));
+        }
+
+        [Test]
+        public void When_Transform_Message_On_Rule_Should_Handle_Message_On_Subscriber()
+        {
+            var messagebus = new UniRxMessageBus(true);
+
+            var handler = new SomeMessageHandler(string.Empty);
+
+            messagebus.AddRule(MessageRouteRule.Create<SomeOtherMessage, SomeMessage, SomeMessageHandler>(string.Empty, false,
+                false, null, new SampleTransformer(), null));
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(handler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => handler.Handle(msg)));
+
+            var message = new SomeOtherMessage("First message", null, DateTime.UtcNow);
+
+            messagebus.RaiseEvent(message);
+
+            Assert.AreEqual(1, handler.HandleCallCount);
+        }
+
+        [Test]
+        public void When_Subscribe_Message_Type_Rule_And_Raise_Drived_Message_Type_Should_Not_Handler_Execute()
+        {
+            var messagebus = new UniRxMessageBus(false);
+
+            var handler = new SomeMessageHandler(string.Empty);
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(handler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => handler.Handle(msg)));
+
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(string.Empty, false, false));
+
+            var message = new SomeDrivedMessage("First message", null, DateTime.UtcNow);
+
+            messagebus.RaiseEvent(message);
+
+            Assert.AreEqual(0, handler.HandleCallCount);
+        }
+
+        [Test]
+        public void When_Subscribe_Message_Type_Rule_And_Raise_Drived_Message_Type_Should_Handler_Execute()
+        {
+            var messagebus = new UniRxMessageBus(false);
+
+            var handler = new SomeMessageHandler(string.Empty);
+
+            messagebus.Subscribe<SomeMessageHandler, SomeMessage>(handler,
+                new MessageHandlerActionExecutor<SomeMessage>(msg => handler.Handle(msg)));
+
+            messagebus.AddRule(MessageRouteRule.Create<SomeMessage, SomeMessageHandler>(string.Empty, true, false));
+
+            var message = new SomeDrivedMessage("First message", null, DateTime.UtcNow);
+
+            messagebus.RaiseEvent(message);
+
+            Assert.AreEqual(1, handler.HandleCallCount);
+        }
+
+        private string CreateAssetMessage(SomeMessageHandler handler, int indent)
+        {
+            var strBuilder = new StringBuilder();
+
+            strBuilder.Append($"\n{new string(' ', indent)}Handler Name: {handler.Name}");
+            strBuilder.Append($"\n{new string(' ', indent)}Handler Type: {handler.Name}");
+            strBuilder.Append($"\n{new string(' ', indent)}Handled Message Count: {handler.HandleCallCount}");
+            strBuilder.Append($"\n{new string(' ', indent)}Handled Messages:");
+            var messages = handler.HandledMessages;
+            foreach (var message in messages)
+            {
+                strBuilder.Append($"\n{new string(' ', indent + 1)}Message: {message.Message}");
+            }
+
+            return strBuilder.ToString();
+        }
+    }
+}
